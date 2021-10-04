b9c8a262858b77878bc3ac2223c9df9e943649b3
Andrew <kolbe1129@gmail.com>
2021-10-04 13:30:14 -0500

    added new and create controller actinos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # the new action instantiates a new article, but does not save it. This article will be used in the view when building the form
+  def new
+    @article = Article.new
+  end
+
+  # the create action instantiates a new article with values for the title and body, and attempts to save it
+  def create
+    @article = Article.new(title: '...', body: '...')
+
+    if @article.save
+      # redirect_to will cause the browser to make a new request, whereas render renders the specified view for the current request
+      # It is important to use redirect_to after mutating the database or application state. 
+      # Otherwise, if the user refreshes the page, the browser will make the same request, and the mutation will be repeated
+      redirect_to article
+    else
+      render :new
+  end
+
 end
