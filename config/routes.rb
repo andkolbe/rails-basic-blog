199d7dc15757777b2d5a8337ec50d5311d417b09
Andrew <kolbe1129@gmail.com>
2021-10-04 15:24:17 -0500

    added a comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
   resources :articles do
     resources :comments # this creates comments as a nested resource within articles
-
+  
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
