4eee436394191844a1ee5b7632f4944ee0d9d5bf
Andrew <kolbe1129@gmail.com>
2021-10-04 12:58:38 -0500

    added a path to get one article by id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,11 @@ class ArticlesController < ApplicationController
   # when an action does not explicity render a view (or otherwise trigger a HTTP response), Rails will automatically render a view that matches the name 
   # of the controller and action. Convention over Configuration
   def index
+    # @articles is called a controller instance variable. They can be accessed by the view
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # display this on the root path "/"
   root "articles#index"
-
   # GET /articles requests are mapped to the index action of ArticlesController
   get '/articles', to: 'articles#index'
+
+  get '/articles/:id', to: "articles#show"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
