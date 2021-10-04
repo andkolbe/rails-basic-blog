188ea24a00dcbfb92f757aab6585bf78a63bd1cd
Andrew <kolbe1129@gmail.com>
2021-10-04 13:09:43 -0500

    refactored article path with link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   # display this on the root path "/"
   root "articles#index"
-  # GET /articles requests are mapped to the index action of ArticlesController
-  get '/articles', to: 'articles#index'
-
-  get '/articles/:id', to: "articles#show"
+  
+  resources :articles
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
