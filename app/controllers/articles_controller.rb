0b6d2fe8ae6efdeb40ad21590bcca0f2c416c381
Andrew <kolbe1129@gmail.com>
2021-10-04 11:41:32 -0500

    added articles route

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,6 @@
+class ArticlesController < ApplicationController
+  # when an action does not explicity render a view (or otherwise trigger a HTTP response), Rails will automatically render a view that matches the name 
+  # of the controller and action. Convention over Configuration
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  # GET /articles requests are mapped to the index action of ArticlesController
+  get '/articles', to: 'articles#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
